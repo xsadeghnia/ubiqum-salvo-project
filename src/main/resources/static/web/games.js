d4ca34b2c113156d4ae091913cc67f831c490561
Afsaneh Sadeghnia <x.sadeghnia@gmail.com>
2020-05-03 15:25:39 +0200

    Afi's changes.

--- src/main/resources/static/web/games.js
+++ src/main/resources/static/web/games.js
@@ -45,7 +45,7 @@
         },
         convertTimestamp:function(t) {
             var date = new Date(t *1000);
-            var formattedDate = ('0' + date.getDate()).slice(-2) + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + date.getFullYear() + ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
+            var formattedDate = ('0' + date.getDate()).slice(-2) + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + ' ' + ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
             return formattedDate
         },
         logIn: async function(){
